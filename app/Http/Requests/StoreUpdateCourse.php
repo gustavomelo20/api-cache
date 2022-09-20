91e3f2d4162bf97b1233a31969ecd08f9a0a6b4d
Gustavo Melo <infogustavomelo@gmail.com>
2022-09-20 17:34:31 -0300

    module course

--- app/Http/Requests/StoreUpdateCourse.php
+++ app/Http/Requests/StoreUpdateCourse.php
@@ -23,8 +23,11 @@ class StoreUpdateCourse extends FormRequest
      */
     public function rules()
     {
+
+        $uuid = $this->course ?? '';
+
         return [
-            'name' => ['required', 'min:3', 'max:255', 'unique:courses'],
+            'name' => ['required', 'min:3', 'max:255', "unique:courses,name,{$uuid},uuid"],
             'description' => ['nullable', 'min:3', 'max:9999']
         ];
     }
--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -25,4 +25,23 @@ class CourseService
         return $this->repository->storeNewCourse($data);
     }
     
+
+    public function getCourse(string $identify) 
+    {
+        return $this->repository->getCourseByUuid($identify);
+    }
+
+
+    public function deleteCourse(string $identify) 
+    {
+        return $this->repository->deleteCourseByUuid($identify);
+    }
+
+
+    public function updateCourse(string $identify, array $data)
+    {
+        return $this->repository->updateCourseByUuid($identify, $data);
+    }
+    
+    
 }
